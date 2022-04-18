3f884be88f5904c609d204769678200a74fda53e
cxy <2987387507@qq.com>
2022-04-18 17:41:13 +0800

    first gitDemo

---
+++ src/main/java/com/xiao/maven/Hello.java
@@ -0,0 +1,8 @@
+package com.xiao.maven;
+
+public class Hello {
+
+  public void show(){
+      System.out.println("Hello Maven World!!");
+  }
+}
---
+++ src/test/java/com/xiao/maven/HelloTest.java
@@ -0,0 +1,14 @@
+package com.xiao.maven;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+    @Test
+    public void testHello(){
+        Hello hello =new Hello();
+
+        hello.show();//Hello Maven World!!
+
+    }
+}
