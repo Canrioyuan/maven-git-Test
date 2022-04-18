2a380fa064d19312d945d8a81eda98d68728bc19
cxy <2987387507@qq.com>
2022-04-18 20:14:19 +0800

    push test gitDemo

--- src/main/java/com/xiao/maven/Hello.java
+++ src/main/java/com/xiao/maven/Hello.java
@@ -6,5 +6,6 @@ public class Hello {
       System.out.println("Hello Maven World!!");
       System.out.println("Hello Maven World!!---git1");
       System.out.println("Hello Maven World!!---Hotfix");
+      System.out.println("PUSH-TEST");
   }
 }
